b20e1e901984187aba5f5878b1895ff240f7f264
Bruno Vedoveto Leandro <brunoovedoveto@gmail.com>
2018-05-22 20:35:48 -0300

    Create request to insert new curriculum to database

---
+++ backend/src/br/unesp/rc/curriculumGenerator/controller/command/NewCurriculum.java
@@ -0,0 +1,34 @@
+package br.unesp.rc.curriculumGenerator.controller.command;
+
+import br.unesp.rc.curriculumGenerator.controller.helper.Helper;
+import br.unesp.rc.curriculumGenerator.model.Curriculum;
+import br.unesp.rc.curriculumGenerator.service.CurriculumService;
+import br.unesp.rc.curriculumGenerator.service.FactoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class NewCurriculum implements ICommand {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String JSONRequest = Helper.getJSONfromHttpExchange(httpExchange);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Curriculum curriculum = objectMapper.readValue(JSONRequest, Curriculum.class);
+
+        int userId = curriculum.getUser().getIdUser();
+
+        CurriculumService curriculumService = FactoryService.getCurriculumService();
+        int curriculumId = curriculumService.insertCurriculum(curriculum, userId);
+
+        //Set the "curriculumId" as response to the frontend
+        String response = Integer.toString(curriculumId);
+
+        httpExchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream outputStream = httpExchange.getResponseBody();
+        outputStream.write(response.getBytes());
+        outputStream.close();
+    }
+}
--- backend/src/br/unesp/rc/curriculumGenerator/controller/servlet/SrvController.java
+++ backend/src/br/unesp/rc/curriculumGenerator/controller/servlet/SrvController.java
@@ -1,9 +1,6 @@
 package br.unesp.rc.curriculumGenerator.controller.servlet;
 
-import br.unesp.rc.curriculumGenerator.controller.command.DownloadCurriculum;
-import br.unesp.rc.curriculumGenerator.controller.command.GetCurriculumList;
-import br.unesp.rc.curriculumGenerator.controller.command.SignIn;
-import br.unesp.rc.curriculumGenerator.controller.command.UserRegistration;
+import br.unesp.rc.curriculumGenerator.controller.command.*;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -16,6 +13,7 @@ public class SrvController {
         server.createContext("/curriculums", new GetCurriculumList());
         server.createContext("/login", new SignIn());
         server.createContext("/registration", new UserRegistration());
+        server.createContext("/newcurriculum", new NewCurriculum());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
